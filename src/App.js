03bf47f710ab57ed1984e13844d08c1949c2173a
Karsenh <KarsenHansen@gmail.com>
2021-05-24 14:26:10 -0700

    Display json data based on button clicked with useEffect fetch change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [resourceType, setResourceType] = useState('Posts');
+  const [items, setItems] = useState([]);
+
+  console.log('Rendered!');
+
+  /**
+   *   useEffect(() => {
+    console.log('On Mount');
+    // Empty array == onMount
+  }, []);
+
+  useEffect(() => {
+    console.log('Resource updated');
+    // Passing in array of values to watch for change
+  }, [resourceType]);
+   * 
+   */
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json));
+  }, [resourceType]);
 
   return (
     <>
       <div>
-        <button onClick={() => setResourceType('Posts')}>Posts</button>
-        <button onClick={() => setResourceType('Users')}>Users</button>
-        <button onClick={() => setResourceType('Comments')}>Comments</button>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })}
     </>
   );
 }
